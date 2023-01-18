0737a008a14e0cbf3470fb655788a931154020ac
BreakingDev24 <102031294+BreakingDev24@users.noreply.github.com>
2023-01-18 16:02:05 +0100

    added counter interaction

--- assets/js/couter-interaction.js
+++ assets/js/couter-interaction.js
@@ -3,3 +3,26 @@ let incrementButton = document.getElementById('increment-button');
 let decrementButton = document.getElementById('decrement-button');
 let resetButton = document.getElementById('reset-button');
 let displayNumber = document.getElementById('display-number');
+
+//set counter to initial value
+
+let counter = 0;
+
+//event listener: increment, decrement, reset
+
+incrementButton.addEventListener('click', ()=>{
+    counter++;
+    displayNumber.innerText = counter
+});
+
+decrementButton.addEventListener('click', ()=>{
+    counter--;
+    displayNumber.innerText = counter
+})
+
+resetButton.addEventListener('click', ()=>{
+    if(window.confirm('Do you want reset the counter?')) {
+        counter = 0
+        displayNumber.innerText = counter
+    }
+})
