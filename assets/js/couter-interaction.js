b3efd3ecbebb88854ea339252317e468e724f4a6
BreakingDev24 <102031294+BreakingDev24@users.noreply.github.com>
2023-01-18 15:54:41 +0100

    create counter interactio variable

---
+++ assets/js/couter-interaction.js
@@ -0,0 +1,5 @@
+//variables 
+let incrementButton = document.getElementById('increment-button');
+let decrementButton = document.getElementById('decrement-button');
+let resetButton = document.getElementById('reset-button');
+let displayNumber = document.getElementById('display-number');
